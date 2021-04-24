98d313167ecbfe20d721d995923545a9b0adb61d
LBU <noeliden1@gmail.com>
2021-04-24 02:15:21 -0400

    secret information secured

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 const port = 5000;
+const config = require('./config/key')
+const { User } = require('./models/User');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://wooklee:abcd1234@web-boiler-plate.z9xkx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -14,5 +19,15 @@ mongoose.connect('mongodb+srv://wooklee:abcd1234@web-boiler-plate.z9xkx.mongodb.
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({ success: true })
+    })
+})
+
+
 app.listen(port, () => console.log(`app listening on port ${port}`))
 
