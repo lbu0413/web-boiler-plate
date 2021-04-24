56327420277ea77865617c30552f507639854d23
LBU <noeliden1@gmail.com>
2021-04-24 02:51:20 -0400

    token created using jwt

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 5000;
-const config = require('./config/key')
+const config = require('./config/key');
 const { User } = require('./models/User');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -25,6 +27,34 @@ app.post('/register', (req, res) => {
     user.save((err, userInfo) => {
         if(err) return res.json({ success: false, err })
         return res.status(200).json({ success: true })
+    })
+})
+
+app.post('/login', (req, res) => {
+    // find requested email from db
+    // if requested email exists in the db, check if password is accurate.
+    // if password is accurate, generate token
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                message: 'no such user corresponding to provided email'
+            })
+        }
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) {
+                return res.json({ loginSuccess: false, message: 'wrong password' })
+            } 
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+                
+                // save as a cookie
+                res.cookie('x_auth', user.token)
+                .status(200)
+                .json({ loginSuccess: true, userId: user._id })
+
+            })
+        })
     })
 })
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -29,7 +33,41 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+userSchema.pre('save', function(next) {
+    var user = this;
+    // bcrypt
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+})
 
-const User = mongoose.model('User', userSchema)
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    //generate token using jsonwebtoken
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token;
+    user.save(function (err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    });
+}
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = { User }
